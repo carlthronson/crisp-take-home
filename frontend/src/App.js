7c8edb7dd02873ce4db1e78cd015a89b9406c751
Carl Thronson <carlthronson@gmail.com>
2024-12-04 17:10:31 -0800

    More cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Accounts from './components/Accounts';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Accounts></Accounts>
-      </div>
-    </Router>
+    <div className="App">
+      <Accounts></Accounts>
+    </div>
   );
 }
 
